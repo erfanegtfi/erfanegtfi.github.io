1cd85144c7a2a8d89e08455d161cd81923a2a95f
Erfan Eghterafi <erfan.eghtrafi@gmail.com>
2025-06-10 17:47:08 +0330

    updated apis

--- radio-mosbat/flutter_service_worker.js
+++ radio-mosbat/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9565adc54ce94ac72a8423261bf2c9a4
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5454b7a8d24c2c39f98e93b71304cc5c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9f4d5ff17e6201a25c0c0e966a9f9611",
+"flutter_bootstrap.js": "8318fc59f3142866d439bc530536ade1",
 "icons/Icon-192.png": "634c12a104339a5d526e1ee79fe4551f",
 "icons/Icon-512.png": "5c530bb4c2510fbe91dcf9db42104e7c",
 "icons/Icon-maskable-192.png": "5c530bb4c2510fbe91dcf9db42104e7c",
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9565adc54ce94ac72a8423261bf2c9a4
 "index.html": "17b438b37bda5074423e0e2114edc8dd",
 "/": "17b438b37bda5074423e0e2114edc8dd",
 "loader.css": "14beb1d6839de6646049a310e2e098c5",
-"main.dart.js": "7bc5baef78dadc4c14db8419bec6a18b",
+"main.dart.js": "daee9b0d15817c406b21342a8d482247",
 "manifest.json": "c22f6cbfa11b57771d558de01fdbc558",
 "version.json": "712ceb48de36545cd754a1e90cad67bc"};
 // The application shell files that are downloaded before a service worker can
